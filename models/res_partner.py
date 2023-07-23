4583d1ec537c99b15683f015490dbcb9372860f0
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2023-07-23 14:42:40 +0600

    activity plan form view and tree view set

--- models/res_partner.py
+++ models/res_partner.py
@@ -10,27 +10,34 @@ class ResPartner(models.Model):
     start_time = fields.Float('Starting Time')
     end_time = fields.Float('Ending Time')
     agent_name = fields.Many2one('res.company', "Agent Name")
-    legend = fields.Many2one('legend.activities', "Legend Activity")
-
+    legend = fields.Many2one('activities.legendactivity', "Activity")
+    activities_ids = fields.One2many("activities.agent", 'stuff_id', "Stuff ID")
 
 class CompanyName(models.Model):
     _inherit = "res.company"
 
 class AgentActivities(models.Model):
-    _name ="agent.activities"
+    _name ="activities.agent"
     _description = "Agent Activities"
     name = fields.Char("Agent")
     description = fields.Char("Agent Activities")
+    stuff_id = fields.Many2one('res.partner', "Stuff")
+    agent_name = fields.Many2one('res.company', "Agent")
+    legend = fields.Many2one('activities.legendactivity', "Activity")
+    activity_date = fields.Date('Date')
+    start_time = fields.Float('Starting Time')
+    end_time = fields.Float('Ending Time')
+
 
 class LegendActivities(models.Model):
-    _name ="legend.activities"
-    _description = "Legend Activities"
-    name = fields.Char("Legend")
-    description = fields.Char("Agent Activities")
+    _name ="activities.legendactivity"
+    _description = "Activities Legend Activities"
+    name = fields.Char("Legend Activity")
+    description = fields.Char("Legend Activities")
 
 class ActivitiesPlanning(models.Model):
     _name ="activities.planning"
     _description = "Activities Planning"
     name = fields.Many2one('res.partner', "Name")
-    agent_name = fields.Many2one('res.company', "Agent Name")
+    agent_name = fields.Many2one('res.partner', "Agent Name")
     description = fields.Char("Activities Planning")
